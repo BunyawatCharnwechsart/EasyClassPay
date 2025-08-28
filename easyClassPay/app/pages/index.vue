a91eaff31df60d3d7c34792b9f3b4cc7a0ac24cc
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2025-08-28 19:42:21 +0700

    login fix

--- easyClassPay/app/pages/index.vue
+++ easyClassPay/app/pages/index.vue
@@ -1,82 +1,104 @@
 <template>
-    <div class="login-box">
-        <h2>เข้าสู่ระบบ</h2>
+    <div class="BG">
+        <div class="login-box">
+        <h1>เข้าสู่ระบบ</h1>
         <form action="" method="post">
+            <label for="username">ชื่อผู้ใช้ /อีเมล /หมายเลขโทรศัพท์</label>
             
-            <span>ชื่อผู้ใช้ /อีเมล /หมายเลขโทรศัพท์</span>
             <input type="text" name="username" required>
+            <label for="password">รหัสผ่าน</label>
 
-            <span>รหัสผ่าน</span>
             <input type="password" name="password" required>
+            <NuxtLink to="/remember">ลืมรหัสผ่าน</NuxtLink>
 
-            <span>ลืมรหัสผ่าน</span>
             <input type="submit" value="เข้าสู่ระบบ">
+            <div class="Register-text">
+                <NuxtLink to="/register">สมัครสมาชิก</NuxtLink>
+            </div>
         </form>
+        </div>
     </div>
 </template>
 
 <style>
 
-    body {
-        font-family: Arial, sans-serif;
-        background-image: url(/background.png);
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+.BG {
+      font-family: Arial, sans-serif;
+      background-image: url(/background.png);
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      position: static;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      height: 100vh;
+      padding-right: 150px;
+      margin: 0;
     }
 
     .login-box {
-        background: rgba(255, 255, 255, 0.2); /* โปร่งใสเล็กน้อย */
-        backdrop-filter: blur(10px);
-        padding: 50px;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0,0,0,0.1);
-        width: 300px;
-        height: 450px;
-        margin: 15% 15% 15% 100% ;
+      background: rgba(255, 255, 255, 0.2); /* โปร่งใสเล็กน้อย */
+      border: 2px solid #000;
+      padding: 40px 30px;
+      box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.2);
+      width: 400px;
+      height: 650px;
+      box-sizing: border-box;
+      color: #000;
+      margin: 0; 
     }
 
-    .login-box h2 {
-        text-align: center;
-        margin-bottom: 20px;
+    .login-box h1 {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+    .login-box label {
+      display: block;
+      margin-top: 15px;
+      font-size: 0.9em;
     }
 
     .login-box input[type="text"],
     .login-box input[type="password"] {
-        width: 80%;
-        padding: 10px;
-        margin: 10px 0;
-        border: 2px solid #ccc;
-        border-radius: 5px;
+      width: 100%;
+      height: 50px;
+      padding: 10px;
+      margin-top: 8px;
+      margin-bottom: 15px;
+      border: 2px solid #ccc;
+      border-radius: 5px;
+      box-sizing: border-box;
+      font-size: 1em;
     }
 
-      .login-box input[type="submit"] {
-        width: 100%;
-        height: 50px;
-        padding: 10px;
-        background-color: #159448;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-weight: bold;
+    .login-box input[type="submit"] {
+      width: 100%;
+      height: 50px;
+      padding: 10px;
+      margin-top: 10px;
+      background-color: #159448;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      box-sizing: border-box;
+      font-size: 1em;
     }
-
-    .login-box .register-link {
+    form a {
+        text-decoration:none ;
+        color:red;
+        text-align: center;
+        margin: 40%;
+        font-size: 0.9em;
+    }
+    .Register-text{
+        text-decoration: none;
         text-align: center;
         margin-top: 15px;
         font-size: 0.9em;
     }
-
-      .login-box .register-link a {
-        color: #007bff;
-        text-decoration: none;
+    .Register-text a{
+        color: #000;
     }
-
-    .login-box .register-link a:hover {
-        text-decoration: underline;
-    }
+    
 </style>
