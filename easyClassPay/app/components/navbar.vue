d6ce0a1babc39a3866bdd1c0cca0bc0fa391774b
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2025-08-30 16:03:28 +0700

    Create footer and createBill

--- easyClassPay/app/components/navbar.vue
+++ easyClassPay/app/components/navbar.vue
@@ -1,20 +1,24 @@
 <template>
 
     <nav class="nav-bar">
+
         <div class="head">
             <img src="/LOGO.png" alt="LOGO">
-            <p class="head-text">Easy Class Pay</p>
+            <nuxt-link to="/main"><h1>Easy Class Pay</h1></nuxt-link>
         </div>
+
         <ul class="list-menu">
             <li><nuxt-link to="/createBill">สร้างบิลเก็บตังค์</nuxt-link></li>
             <li><nuxt-link to="/printYourSelf">พิมพ์รายการเอง</nuxt-link></li>
             <li><nuxt-link to="/monthlyBill">บิลแบบรายเดือน</nuxt-link></li>
             <li><nuxt-link to="/allBill">บิลทั้งหมด</nuxt-link></li>
         </ul>
+
         <div class="notification">
             <img class="bell" src="/bell.png" alt="bell">
             <img class="profile" src="/testProfile.png" alt="profile">
         </div>
+
     </nav>
 
 </template>
@@ -32,92 +36,93 @@ useHead({
 
 <style>
       
-* {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-    align-content: center;
-    font-family: 'Bai Jamjuree', sans-serif;
-}
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+        align-content: center;
+        font-family: 'Bai Jamjuree', sans-serif;
+    }
 
-.nav-bar {
-    display: flex;
-    padding: 10px;
-    align-items: center;
-    background-color: #FFC93F;
-}
+    .nav-bar {
+        display: flex;
+        padding: 10px;
+        align-items: center;
+        background-color: #FFC93F;
+    }
 
-.head {
-    display: flex;
-    gap: 40px;
-}
+    .head {
+        display: flex;
+        gap: 40px;
+    }
 
-.head img {
-    width: 50px;
-    height: auto;
-    margin-left: 30px;
-}
+    .head img {
+        width: 50px;
+        height: auto;
+        margin-left: 30px;
+    }
 
-.head-text {
-    font-size: 24px;
-    font-weight: bold;
-}
+    .head a {
+        text-decoration: none;
+        font-size: 28px;
+        color: black;
+    }
 
-.list-menu {
-    display: flex;
-    padding-left: 40px;
-    gap: 20px;
-}
+    .list-menu {
+        display: flex;
+        padding-left: 40px;
+        gap: 60px;
+    }
 
-.list-menu li {
-    color: #fff;
-    display: grid;
-    place-content: center;
-    list-style: none;
-}
+    .list-menu li {
+        color: #fff;
+        display: grid;
+        place-content: center;
+        list-style: none;
+    }
 
-.list-menu li a {
-    position: relative;
-    color: black;
-    text-decoration: none;
-    font-size: 18px;
-    font-weight: bold;
-    transition: all 0.3s ease;
-}
-    
-.list-menu li a::before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    background: black;
-    border-radius: 5px;
-    transform: scaleX(0);
-    transition: transform 0.5s ease;
-    bottom: 0;
-    left: 0;
-    transform-origin: left;
-}
+    .list-menu li a {
+        position: relative;
+        color: black;
+        text-decoration: none;
+        font-size: 18px;
+        font-weight: bold;
+        transition: all 0.3s ease;
+    }
+        
+    .list-menu li a::before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        background: black;
+        border-radius: 5px;
+        transform: scaleX(0);
+        transition: transform 0.5s ease;
+        bottom: 0;
+        left: 0;
+        transform-origin: left;
+    }
 
-.list-menu li a:hover::before {
-    transform: scaleX(1);
-}
+    .list-menu li a:hover::before {
+        transform: scaleX(1);
+    }
 
-.notification{
-    display: flex;
-    gap: 30px;
-    margin-left: auto;
-    margin-right: 30px;
-}
+    .notification{
+        display: flex;
+        gap: 30px;
+        margin-left: auto;
+        margin-right: 30px;
+    }
 
-.notification img {
-    align-self: center;
-    cursor: pointer;
-}
+    .notification img {
+        align-self: center;
+        cursor: pointer;
+    }
 
-.bell {
-    width: 30px;
-    height: 30px;
-}
+    .bell {
+        width: 30px;
+        height: 30px;
+    }
 
 </style>
