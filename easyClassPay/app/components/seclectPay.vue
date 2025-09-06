f0e76325a211de8f30f40ac2dc5a12024775cc19
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2025-09-06 15:15:01 +0700

    seclectPay

--- easyClassPay/app/components/seclectPay.vue
+++ easyClassPay/app/components/seclectPay.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="boxPayMent">
-    <div class="headbar"><p>เลือกวิธีชำระเงิน</p></div>
-    
-  </div>
+    <div class="boxPayMent">
+        <div class="headbar"><p>เลือกวิธีชำระเงิน</p></div>
+        
+        <div class="PayMent-btn">
+            <nuxt-link to="/total-bill">ถัดไป <img src="/arrow-r.png"></nuxt-link>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -24,5 +27,31 @@
         text-align: center;
         font-size: 30px;
         font-weight: bold;
+    }
+
+    .PayMent-btn {
+        display: flex;
+        margin: 30px;
+        margin-top: auto;      /* ดันปุ่มไปล่างสุด */
+    }
+
+    .PayMent-btn a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        width: 100%;
+        border: none;
+        border-radius: 10px;
+        height: 45px;
+        background: #159448;
+        color: white;
+        font-size: 18px;
+        font-weight: bold;
+        text-decoration: none;
+    }
+
+    .PayMent-btn a img {
+        margin-left: 5px;
     }
 </style>
