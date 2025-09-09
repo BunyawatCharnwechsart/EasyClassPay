a63cb779d68086962946f84fa91c7de43a831258
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2025-09-09 17:17:59 +0700

    ADD createBill
    พึ่งทำ

---
+++ easyClassPay/app/pages/createBill.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <navbar />
+        <div>
+            
+        </div>
+        <about />
+    </div>
+</template>
+
+<script setup>
+import navbar from '~/components/navbar.vue'
+import about from '~/components/about.vue';
+</script>
