26e3812e9b01e63e93c2f40e52ae613707448b85
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2025-09-15 14:34:49 +0700

    Add allBill manualBill monthBill Component payMent
    ยังไม่เสร็จ all manualBill monthBill

---
+++ easyClassPay/app/pages/allBill.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="font-Bai-Jamjuree">
+        <navbar/>
+    </div>
+</template>
+
+<script setup>
+import navbar from '~/components/navbar.vue';
+import about from '~/components/about.vue';
+</script>
